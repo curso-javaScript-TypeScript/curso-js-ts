66f1d2c3c209036dcf0ca28db5a15325d1a8df42
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-19 21:54:54 -0300

    Finalizando aula 4 sobre comentários em linha ou parte e em blocos ou blocos

---
+++ aula4/comentários.js
@@ -0,0 +1,24 @@
+// É escrever um comentário
+console.log('Deus é fiel!!'); // Aqui tem outro comentário
+console.log('Linha 1'); // E estes comentários não aparecerá no terminal
+console.log('Linha 2'); // ou seja serã ignorados
+console.log('Linha 3'); // serve para compreensão dos códigos pós criação
+console.log('Linha 4'); // serve tb para outros dev1's compreender
+// console.log('Linha 5'); pode ser parte ou a linha inteira
+console.log('Linha 5');
+
+
+
+/* 
+acima tinhamos comentários por linha
+com este asterisco e barra
+temos o comentário em blocos
+ou seja, tudo tb será ignorado
+no terminal
+*/
+
+// linha ou parte dela
+
+/*
+bloco de código ou códigos
+*/
