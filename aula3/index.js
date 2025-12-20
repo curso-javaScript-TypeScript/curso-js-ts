4406cba60464f5673bb85a9e3640ebe300408adf
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-19 21:42:42 -0300

    Finalizando aula 3 com além de conhecer 'console.log' tem um exercício

--- aula3/index.js
+++ aula3/index.js
@@ -1,1 +1,9 @@
-console.log();
+console.log('Magno V Gomes'); // String = Texto
+console.log("'Magno' V Gomes");
+console.log("Magno V Gomes");
+console.log('"Magno" V Gomes');
+console.log(`Magno V Gomes`);
+console.log(`'Magno' V "Gomes"`);
+
+console.log( 26.03, 1974); // Number = Número
+console.log(51, 'anos de idade', 26.03, 'dia e mês do nascimento');
