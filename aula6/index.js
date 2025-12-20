1483e6057f3af53b47a2f66bfbf58342ac7adf46
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-20 14:46:02 -0300

    Finalizando aula 6 - várias regras sobre uso de variáveis

--- aula6/index.js
+++ aula6/index.js
@@ -1,5 +1,31 @@
-console.log('João nasceu em 1984.');
-console.log('Em 2000 João conheceu Maria.');
-console.log('João casou-se com Maria em 2012.');
-console.log('Maria teve 1 filho com João em 2015.');
-console.log('O filho de Joáo se chama Eduardo.');
+/* Não podemos criar variáveis com palavras reservadas
+como let = let, por exemplo.
+variáveis precisam ter nomes significativos
+Não pode começar o nome de uma variavél com números
+Não pode conter espaços ou traços => nome cliente - nome-cliente
+Utilizamos camelCase => nomeCompletoDoCliente
+letras maiúsculas e minusculas fazem diferencas => nome - Nome
+não podemos redeclarar variávies com let
+NÃO UTILIZE VAR, UTILIZE LET.
+*/
+let nomeCliente = 'Magno';
+nomeCliente = 'Neusa';
+
+console.log(nomeCliente) // imprime = Neusa
+
+// let nome; // Apenas declarou a variável
+// nome = 'João'; // Inicializando a variável
+// console.log(nome);
+
+// nome = 'Otávio';
+// console.log(nome);
+
+
+// let nome = 'Henrique'; // String
+//  // var nome = 'João' tem quase a mesma finalidade mais é antiga.
+
+// console.log(nome, 'nasceu em 1984.');
+// console.log('Em 2000', nome, 'conheceu Maria.');
+// console.log(nome,'casou-se com Maria em 2012.');
+// console.log('Maria teve 1 filho com',nome, 'em 2015.');
+// console.log('O filho de',nome, 'se chama Eduardo.');
