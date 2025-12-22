a19327eaa4a601df59a9317fbe562b723ab2e707
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-22 16:13:47 -0300

    Finalizando aula 11 com um exercicio feito no vscode e a resposta na pág web

--- aula011/js/script.js
+++ aula011/js/script.js
@@ -1,1 +1,11 @@
-alert('Deus é fiel. Confie sempre nele!');
+// alert('Deus é fiel. Confie sempre nele!');
+
+let num1 = prompt('Insira um número');
+let num2 = prompt('Insira outro número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const soma = num1 + num2;
+
+alert(`O resultado da sua conta foi: ${soma}`);
