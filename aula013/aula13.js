f98592921b732e4559c16ada7c039a8353f685a4
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-22 19:31:57 -0300

    Finalizando aula 13 - conhecendo e testando as funcionalidade s do string

--- aula013/aula13.js
+++ aula013/aula13.js
@@ -1,3 +1,25 @@
-let umaString = 'Apenas um "texto"';
+// Strings são indexadas, ou seja, tem indíces em cada caractere
 
-console.log(umaString);
+
+//               01234567 <- indices
+let String1 = "Um texto";
+console.log(String1.concat(' ', 'maior')); // unido dois textos
+console.log(String1+ ' maior!'); // unido dois textos
+console.log(`${String1} maior.`); // unido dois textos
+
+
+let umaString = "Um texto";
+console.log(umaString[4]); // resp -> e
+console.log(umaString[8]); // resp -> undefined
+console.log(umaString[7]); // resp -> o
+
+console.log(umaString.charAt(6)); // resp -> t
+console.log(umaString.charAt(8)); // resp -> valor vazio
+
+
+// let umaString = 'Apenas um "texto"';
+// let outraString = "Apenas um \"texto\"";
+// // barra invertida serve como caractere de escape
+// // no caso de ter que usar duas aspas duplas
+// console.log(umaString);
+// console.log(outraString);
