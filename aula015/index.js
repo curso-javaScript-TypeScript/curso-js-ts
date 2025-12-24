32dad0ce287bd71cf94496878152835076e33538
Magno V Gomes <magnovgomes74@gmail.com>
2025-12-24 18:23:11 -0300

    Finalizando aula 15 - colocando em práticas várias funções do obj 'Math'

--- aula015/index.js
+++ aula015/index.js
@@ -1,1 +1,21 @@
-Math
+console.log(100 / 0); // não é 0 e sim infinity, assim é True
+
+// let num1 = 9.54578;
+
+// let num2 = Math.floor(num1); arredonda para baixo
+// let num2 = Math.ceil(num1); arredonda para cima
+// let num2 = Math.round(num1); arredonda automáticamente
+// console.log(num2);
+
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); maior nº
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); menor nº
+
+// const aleatorio = Math.random(); numero aleatório
+// const aleatorio = Math.random() * (10 - 5) + 10;
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 10);
+// console.log(aleatorio);
+
+// let num1 = 9;
+// console.log(num1 ** 0.5);
+// let num2 = 9;
+// console.log(num2 ** (1/2));
