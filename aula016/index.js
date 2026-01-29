33f6f2652c3da27bd8e23dc2ccd64cbb67335a59
Magno V Gomes <magnovgomes74@gmail.com>
2026-01-29 20:30:34 -0300

    Finalizando aula 16 - Compreendendo o principio e mudanças em Array

---
+++ aula016/index.js
@@ -0,0 +1,41 @@
+//                0         1         2
+const alunos = ['Magno', 'Neusa', 'Arthur'];
+
+console.log(alunos);
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+
+/*
+console.log(alunos[1]);
+delete alunos[1]; -> excluir aluno
+const removido = alunos.pop(); -> último aluno
+const removido = alunos.shift(); -> primeiro aluno
+alunos.shift();
+alunos.pop();
+console.log(removido);
+
+alunos.unshift('Mariana'); -> adiciona no inicio
+alunos.unshift('Gustavo'); -> adiciona no inicio
+
+alunos.push('Mariana'); -> adiciona no fim
+alunos.push('Gustavo'); -> adiciona no fim
+alunos.push('Laila'); -> adiciona no fim
+
+alunos[alunos.length] = 'Mariana'; -> adiciona no fim
+alunos[alunos.length] = 'Gustavo'; -> adiciona no fim
+alunos[alunos.length] = 'Laila'; -> adiciona no fim
+
+alunos[1] = 'Mariana'; -> altera
+alunos[3] = 'Neusa'; -> adiciona
+
+            01234567891011   etc
+const nome = 'Magno V Gomes'
+const coisas = ['Magno', 'Neusa', 'Arthur', '1', 'True', 'null'];
+console.log(coisas);
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+*/
