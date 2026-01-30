54e5b8bf2c447c53975f51ef0ae414c417f652e7
Magno V Gomes <magnovgomes74@gmail.com>
2026-01-29 21:43:39 -0300

    Finalizando aula 17 - Entendendo algumas formas de declarar funções como: function, => e em uma linha apenas

--- aula017/index.js
+++ aula017/index.js
@@ -1,41 +1,42 @@
-//                0         1         2
-const alunos = ['Magno', 'Neusa', 'Arthur'];
+const raiz = n => Math.sqrt(n);
 
-console.log(alunos);
-console.log(typeof alunos);
-console.log(alunos instanceof Array);
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+console.log(raiz(36));
 
+/* 
+const raiz = (n) => {
+  return Math.sqrt(n);
+};
 
+const raiz = function (n) {
+  return Math.sqrt(n);
+};
 
-/*
-console.log(alunos[1]);
-delete alunos[1]; -> excluir aluno
-const removido = alunos.pop(); -> último aluno
-const removido = alunos.shift(); -> primeiro aluno
-alunos.shift();
-alunos.pop();
-console.log(removido);
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+console.log(raiz(36));
 
-alunos.unshift('Mariana'); -> adiciona no inicio
-alunos.unshift('Gustavo'); -> adiciona no inicio
+AQUI AS FUNÇÕES NÃO PRECISAM DO ';' já quando a função vem pós variável precisa
 
-alunos.push('Mariana'); -> adiciona no fim
-alunos.push('Gustavo'); -> adiciona no fim
-alunos.push('Laila'); -> adiciona no fim
+function agradecimento(nome) {
+  function soma(x, y) {
+    const resultado = x + y;
+    return resultado;
+  }
+  
+  console.log(soma(4, 6));
+  console.log(soma(40, 60));
+  console.log(soma(9999, 88888));
 
-alunos[alunos.length] = 'Mariana'; -> adiciona no fim
-alunos[alunos.length] = 'Gustavo'; -> adiciona no fim
-alunos[alunos.length] = 'Laila'; -> adiciona no fim
+return `Deus é fiel, nunca se esqueça ${nome}`;
+}
 
-alunos[1] = 'Mariana'; -> altera
-alunos[3] = 'Neusa'; -> adiciona
-
-            01234567891011   etc
-const nome = 'Magno V Gomes'
-const coisas = ['Magno', 'Neusa', 'Arthur', '1', 'True', 'null'];
-console.log(coisas);
-console.log(alunos);
-console.log(alunos[0]);
-console.log(alunos[1]);
-console.log(alunos[2]);
+const variavel = agradecimento('Magno');
+console.log(variavel);
+agradecimento('Magno');
+agradecimento('Neusa');
+agradecimento('Mariana');
 */
