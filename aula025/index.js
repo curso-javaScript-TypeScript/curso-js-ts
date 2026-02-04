8e8113e67a4778f334346e273f2f93887223dad9
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-04 16:29:02 -0300

    Finalizando aula 25 - Compreendendo o funcionamento das Est. de Comparações (IF, ELSE IF e ELSE)

--- aula025/index.js
+++ aula025/index.js
@@ -1,7 +1,10 @@
 const numero = 10;
 
-// Se (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
-// Se não faça isso {o código}
+/*
+ Se (numero >= 0 && numero <= 5) ocorrer, faça isso {código}
+ Se não faça isso {o código}
+
+ */
 
 if (numero <= 10) { // Verdadeira
   console.log('O número é menor ou igual a 10.');
@@ -11,7 +14,9 @@ if (numero >= 0 && numero <= 5) {
   console.log('O número está entre 0 e 5.');
 } else if (numero >= 6 && numero <= 8) {
   console.log('O número está entre 6 e 8.');
-} else if (numero >= 9 && numero <= 11) { // Verdadeiro
+//} else if (1 === 1) {
+// console.log('Vai interromper aqui');
+} else if (numero >= 9 && numero <= 11) {
   console.log('O número está entre 9 e 11.');
 } else {
   console.log('O número não está entre 0 e 11.');
