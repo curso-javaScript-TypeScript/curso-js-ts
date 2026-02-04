71fd5ebdcdbe54e23cd23ef412e491d6c953498a
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-03 21:06:08 -0300

    Finalizando aula 22 - sobre operadores lógicos -> AND (&&), OR (||) e NOT (!)

--- aula022/index.js
+++ aula022/index.js
@@ -1,60 +1,23 @@
 /*
 ***** Operadores Lógicos *****
+&& -> AND -> E -> todas expressões precisam ser verdadeiras para retornar True
+console.log(true && true && true && true)
+console.log(true && false && true && true)
+const expressaoAnd = true && true && true && true;
+const expressaoAnd = true && false && true && true;
+console.log(expressaoAnd)
 
->   maior que
-const comp = 10 > 5;
-console.log(comp);
-console.log(10 > 5);
+|| -> OR  -> OU -> Só retorna false se todas as expressões forem falsas
+// const expressaoOr = true || false || false || false;
+// const expressaoOr = false || true || true || true;
+// const expressaoOr = true || true || true || true;
+const expressaoOr = false || false || false || false;
+console.log(expressaoOr);
 
->=  maior que ou igual a
-const comp = 10 >= 5;
-const comp = 10 >= 10;
-const comp = 10 >= 11;
-console.log(comp);
-
-<   menor que
-const comp = 10 < 11;
-const comp = 10 < 10;
-const comp = 10 < 9;
-console.log(comp);
-
-<=  menor que ou igual a
-const comp = 10 <= 11;
-const comp = 10 <= 10;
-const comp = 10 <= 9;
-console.log(comp);
-
-==  igualdade (valor) ********** <- não se utiliza
-const num1 = 10; NUMBER
-const num2 = '10'; STRING
-const comp = num1 == num2; a ling. fez a conversão por conta própria
-console.log(comp); Resultado é True, por isso não devemos usar
-
-=== igualdade estrita (valor e tipo) 
-const num1 = 10; NUMBER
-const num2 = 10; NUMBER
-const num2 = '10'; STRING
-const comp = num1 === num2; agora a ling. leu o valor primitivo
-console.log(comp);
-
-!=  diferente (valor)  ********** <- não se utiliza
-const num1 = 10; mesma situação do (==)
-const num2 = '10';
-const num2 = 10;
-const num2 = 12;
-const comp = num1 != num2;
-console.log(comp);
-
-!== diferente estrito (valor e tipo)
-const num1 = 10; mesma situação (===)
-const num2 = '10';
-const num2 = 10;
-const num2 = 12;
-const comp = num1 !== num2;
-console.log(comp);
+!  -> NOT -> NÃO -> inverte o valor da afirmação, ou seja, negação
+console.log(!true);
+console.log(!false);
 
 */
-const num1 = 10;
-const num2 = 12;
-const comp = num1 !== num2;
-console.log(comp);
+console.log(!true);
+console.log(!false);
