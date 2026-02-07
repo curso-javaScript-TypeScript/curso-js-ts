fe4315e00acc35c1bb7cd5f35347d727d0b6f571
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-07 15:29:22 -0300

    Finalizando aula 27 - Aplicação da 'Operação Ternária (? , : e ||)' em JS

--- aula027/inidex.js
+++ aula027/inidex.js
@@ -1,5 +1,13 @@
-// ? :
-const pontuacaoUsuario = 1000;
+// Condição -> ? <- 'Valor para verdadeiro -> : <- 'Valor para falso'
+const pontuacaoUsuario = 500;
+// const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'usuário normal';
+
+const corUsuario = null;
+// const corUsuario = '/ Pink';
+const corPadrao = corUsuario || '/ Azul';
+console.log(nivelUsuario, corPadrao);
+
 
 
 
