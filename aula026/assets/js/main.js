535c380cf07a9ceaec0fd3e5b6bb8f3edd506d1b
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-06 23:11:18 -0300

    Finalizando aula 26 - exercício sobre IMC englobando HTML, CSS e JavaScript

---
+++ aula026/assets/js/main.js
@@ -0,0 +1,67 @@
+// Capturar evento de submit do formlário
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector('#peso');
+  const inputAltura = e.target.querySelector('#altura');
+  
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResultado('Peso inválido!', false);
+    return;
+  }
+
+  if (!altura) {
+    setResultado('Altura inválida!', false);
+    return;
+  }
+
+  const imc = getImc(peso, altura);
+  const indiceImc = getIndiceImc(imc);
+
+  const msg = `Seu IMC é ${imc} (${indiceImc}).`;
+
+  setResultado(msg, true);
+});
+
+function getIndiceImc (imc) {
+  const indice = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
+    'Obesidade gral 1', 'Obesidade gral 2', 'Obesidade gral 3'];
+
+  if (imc >= 39.9) return indice[5];
+  if (imc >= 34.9) return indice[4];
+  if (imc >= 29.9) return indice[3];
+  if (imc >= 24.9) return indice[2];
+  if (imc >= 18.5) return indice[1];
+  if (imc < 18.5) return indice[0];
+}
+
+function getImc (peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+}
+
+function criaParagrafo () {
+  const p = document.createElement('p');
+  return p;
+}
+
+function setResultado (msg, isValid) {
+  const resultado = document.querySelector('#resultado');
+  resultado.innerHTML = '';
+
+  const p = criaParagrafo();
+  
+  if (isValid) {
+    p.classList.add('paragrafo-resultado');
+  } else {
+    p.classList.add('bad');
+  }
+
+  p.innerHTML = msg;
+  resultado.appendChild(p);
+}
+
