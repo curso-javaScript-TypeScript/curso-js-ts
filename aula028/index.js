e4eea5e2e5b74c734936acbfbe08dd4d7ec1f966
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-07 17:58:32 -0300

    Finalizando aula 28 - Entendendo como usar 'Date' para capturar: data, hora, dia da semana, etc.

--- aula028/index.js
+++ aula028/index.js
@@ -1,1 +1,46 @@
+/*
+const data = new Date(0); -> 01/01/1970 TimesTamp unix ou época unix (marco zero) 
+
+const MarcoZero = 60 * 60 * 3 * 1000;
+const umDia = 60 * 60 * 24 * 1000;
+const data = new Date(0 + MarcoZero - umDia);
+const data = new Date(0 + MarcoZero + umDia);
+console.log(data.toString());
+
+const data = new Date(2026, 1, 7, 15, 53, 45); -> ano, mês, dia, hora, min, seg.
+
+const data = new Date('2026-02-07 16:20:48.999');
+console.log(data.toString());
+
+const data = new Date(1770494230790);
+
+console.log('Dia', data.getDate());
+console.log('Mês', data.getMonth() + 1);
+console.log('Ano', data.getFullYear());
+console.log('Hora', data.getHours());
+console.log('Min', data.getMinutes());
+console.log('Seg', data.getSeconds());
+console.log('miniseg', data.getMilliseconds());
+console.log('Dia semana', data.getDay());
+
+console.log(Date.now());
+*/
+
+function zeroAEsquerda (num) {
+  return num >= 10 ? num : `0${num}`;
+}
+
+function formatarData(data){
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());
+  const min = zeroAEsquerda(data.getMinutes());
+  const seg = zeroAEsquerda(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
 const data = new Date();
+const dataBrasil = formatarData(data);
+console.log(dataBrasil);
