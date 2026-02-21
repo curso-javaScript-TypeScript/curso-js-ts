f31ccb20e22f45cb74a2cf5caa39af8354379f5e
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-21 13:15:08 -0300

    Finalizando aula 35 - compreendendo através de exercício o uso de 'for' dentro do JS

--- aula035/assets/js/main.js
+++ aula035/assets/js/main.js
@@ -1,6 +1,22 @@
 const elementos = [
-  {tag: 'p', texto: 'Frase 1'},
-  {tag: 'div', texto: 'Frase 2'},
-  {tag: 'footer', texto: 'Frase 3'},
-  {tag: 'section', texto: 'Frase 4'},
+  {tag: 'p', texto: 'tag paragráfo (p)'},
+  {tag: 'div', texto: 'tag divisão (div)'},
+  {tag: 'footer', texto: 'tag rodapé (footer)'},
+  {tag: 'section', texto: 'tag sessão (section'},
 ];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+  let {tag, texto} = elementos[i];
+  let tagCapturar = document.createElement(tag); // primeira forma de capturar o loop
+  let textoCaptura = document.createTextNode(texto); // segunda forma de capturar o loop
+
+  tagCapturar.appendChild(textoCaptura);
+  tagCapturar.innerText = texto; // primeira forma de capturar para imprimir o resultado
+  tagCapturar.innerHTML = texto; // segunda forma de capturar para imprimir o resultado
+  div.appendChild(tagCapturar);
+}
+
+container.appendChild(div);
