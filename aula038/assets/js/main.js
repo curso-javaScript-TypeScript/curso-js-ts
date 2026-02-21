eadcdb20dd7c883d86e181cdce414aae8b1c474b
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-21 18:00:51 -0300

    Finalizando aula 38 - Resolução do exercício estilizando os paragrafos direto do JS e não pelo CSS

--- aula038/assets/js/main.js
+++ aula038/assets/js/main.js
@@ -1,1 +1,11 @@
 const paragrafos = document.querySelector('.paragrafos');
+const qtdParagrafos = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+for (let p of qtdParagrafos) {
+  p.style.backgroundColor = backgroundColorBody;
+  p.style.color = '#fff';
+}
