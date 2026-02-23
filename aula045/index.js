e6649f1379733ed54327683c478b97bd974c9f42
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-23 20:50:47 -0300

    Finalizando aula 45 - Tratamento e lançamento de erros, usando funçoes, estr. condicionais, etc em JS

--- aula045/index.js
+++ aula045/index.js
@@ -2,4 +2,54 @@ try {
 // É executada quando não há erros
 } catch (e) {
 // É executada quando há erros
+} finally {
+// Este bloco será executado sempre, podendo ser omitido
 }
+
+function horaAtual(data) {
+  if ( data && !(data instanceof Date)) {
+    throw new TypeError('Esperando instância de Date.');
+  }
+
+  if (!data) {
+    data = new Date();
+  }
+
+  return data.toLocaleTimeString('pt-BR');
+}
+
+
+try {
+  const data = new Date('23-02-2026 20:41:12');
+  const hora = horaAtual();
+  console.log(hora);
+} catch(e) {
+  // Tratar erro aqui
+}
+finally {
+  console.log('\nNovamente o finally')
+}
+
+
+/*
+try {
+  // console.log(erroDeProposito);
+  console.log('Abrir um arquivo');
+  console.log('Manipulei o arquivo e gerou erro');
+  console.log('Fechei o arquivo');
+
+  try {
+    console.log(outroErroProposital);
+  } catch(e) {
+    console.log('\nErro detectado');
+  } finally {
+    console.log('Também sou Finally!');
+  }
+
+
+} catch (e) {
+  console.log('Tratando o erro');
+} finally {
+  console.log('\nFinally: Eu sempre sou executado');
+}
+*/
