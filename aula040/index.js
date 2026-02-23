082ba333e75bffd215962ab45d5eb87a22344670
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-23 15:52:27 -0300

    Finalizando aula 40 - Exercitando o uso de 'continue' o 'break' nas Est. de repetição

--- aula040/index.js
+++ aula040/index.js
@@ -1,1 +1,53 @@
-const
+// Continue continua para próxima iteração
+// Break sai do laço imediatamente
+// Ambos funcionam em: for clássico, for in, for of
+// while (tendo cuidado para atualizar antes) e Do while
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let i = 0;
+while (i < numeros.length) { 
+  let numero = numeros[i];
+
+  if (numero === 2) {
+    console.log(`Pulei o número ${numero}`);
+    i++;
+    continue;
+  }
+
+  console.log(numero);
+
+  if (numero === 6) {
+console.log(`${numero} encontrado, saindo...`);
+    i++;
+    break;
+  }
+
+  i++;
+}
+
+console.log('\n**************\n');
+
+
+const nums = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+let x = 0;
+do {
+  let num = nums[x];
+
+  if (num === 30) {
+    console.log(`Pulei o número ${num}`);
+    x++;
+    continue;
+  }
+
+  console.log(num);
+
+  if (num === 70) {
+console.log(`${num} encontrado, saindo...`);
+    x++;
+    break;
+  }
+
+  x++;
+} while (x < nums.length);
