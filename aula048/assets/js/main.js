5467d634a933e789d66f0ce534994c146a19192e
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-24 16:38:18 -0300

    Atingindo metade das funcionalidades com a criação de functions e inputs

--- aula048/assets/js/main.js
+++ aula048/assets/js/main.js
@@ -1,3 +1,32 @@
 const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
-const tarefa = document.querySelector('.tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLista() {
+  const li = document.createElement('li');
+  return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e) {
+  if (e.keyCode === 13) {
+   if (! inputTarefa.value)  return;
+  criaTarefa(inputTarefa.value); 
+  }
+});
+
+function limpaInput() {
+  inputTarefa.value = '';
+  inputTarefa.focus();
+}
+
+function criaTarefa(textoInput) {
+  const li = criaLista();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  limpaInput();
+}
+
+btnTarefa.addEventListener('click', function() {
+  if (! inputTarefa.value)  return;
+  criaTarefa(inputTarefa.value);
+});
