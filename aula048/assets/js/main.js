7110544995966fa7967db9bbf18dbe4880feee4f
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-24 17:39:27 -0300

    Finalizando aula 48 - Encerrando também seção 3 com a criação de uma lista de tarefas com vários elementos aprendidos até aqui

--- aula048/assets/js/main.js
+++ aula048/assets/js/main.js
@@ -2,7 +2,7 @@ const inputTarefa = document.querySelector('.input-tarefa');
 const btnTarefa = document.querySelector('.btn-tarefa');
 const tarefas = document.querySelector('.tarefas');
 
-function criaLista() {
+function criaLi() {
   const li = document.createElement('li');
   return li;
 }
@@ -19,14 +19,60 @@ function limpaInput() {
   inputTarefa.focus();
 }
 
+function criaBotaoApagar(li) {
+  li.innerText += ' ';
+  const botaoApagar = document.createElement('button');
+  botaoApagar.innerText = 'Apagar';
+  // botaoApagar.clasList.add('apagar');
+  botaoApagar.setAttribute('class', 'apagar');
+  botaoApagar.setAttribute('title', 'apagando tarefa');
+  li.appendChild(botaoApagar);
+}
+
 function criaTarefa(textoInput) {
-  const li = criaLista();
+  const li = criaLi();
   li.innerText = textoInput;
   tarefas.appendChild(li);
   limpaInput();
+  criaBotaoApagar(li) 
+  salvarTarefas();
 }
 
 btnTarefa.addEventListener('click', function() {
   if (! inputTarefa.value)  return;
   criaTarefa(inputTarefa.value);
 });
+
+document.addEventListener('click', function(e) {
+  const el = e.target;
+
+  if (el.classList.contains('apagar')) {
+    el.parentElement.remove();
+    salvarTarefas();
+  }
+});
+
+function salvarTarefas() {
+  const liTarefas = tarefas.querySelectorAll('li');
+  const listaDeTarefas = [];
+
+  for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+    listaDeTarefas.push(tarefaTexto);
+  }
+
+  const tarefasJSON = JSON.stringify(listaDeTarefas);
+  localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function AddTarefasSalvas() {
+  const tarefas = localStorage.getItem('tarefas');
+  const listaDeTarefas = JSON.parse(tarefas);
+  console.log(listaDeTarefas);
+
+  for (let tarefa of listaDeTarefas) {
+    criaTarefa(tarefa);
+  }
+}
+AddTarefasSalvas();
