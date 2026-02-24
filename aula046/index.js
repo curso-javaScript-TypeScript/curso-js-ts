5de1bda8c322bec4ff79b3d238f02d4befabd913
Magno V Gomes <magnovgomes74@gmail.com>
2026-02-23 21:20:47 -0300

    Finalizando aula 46 - Controlando times (setInterval e setTimeout) em JS

--- aula046/index.js
+++ aula046/index.js
@@ -1,3 +1,31 @@
 function mostrarHora() {
-  
+  let data = new Date();
+
+  return data.toLocaleTimeString('pt-BR');
 }
+
+ const tempo = setInterval(function () {
+  console.log(mostrarHora());
+}, 1000);
+
+setTimeout(function() {
+  clearInterval(tempo);
+}, 3000);
+
+setTimeout(function() {
+  console.log('\nDeus é fiel!!!');
+}, 5000);
+
+
+
+
+/* 
+setTimeout(function() {
+  clearInterval(tempo);
+}, 10000);
+
+ ****** intervalo de tempo ******
+setInterval(function () {
+  console.log(mostrarHora());
+}, 5000);
+*/
