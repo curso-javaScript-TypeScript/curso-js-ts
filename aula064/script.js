f4fb6a50b8e4b8733c16cde330513f591bfa68e1
Magno V Gomes <magnovgomes74@gmail.com>
2026-03-05 17:10:57 -0300

    Finalizando aula 64 - concatenação de arrays com .concat() e spread operator (...)

--- aula064/script.js
+++ aula064/script.js
@@ -1,1 +1,24 @@
-const num =  ;
+const aray1 = [1, 2, 3];
+const aray2 = [4, 5, 6];
+const aray3 = [...aray1, 'Magno',...aray2, ...[7, 8, 9]];
+// const aray3 = [...aray1, 'Magno',...aray2, [7, 8, 9]];
+// ...rest -> ... spread;
+console.log(aray3);
+
+
+
+/*
+const aray1 = [1, 2, 3];
+const aray2 = [4, 5, 6];
+const aray3 = aray1.concat(aray2, [7, 8, 9], 'Magno');
+const aray3 = aray1.concat(aray2);
+console.log(aray3);
+
+*****************************************
+
+const aray1 = [1, 2, 3];
+const aray2 = [4, 5, 6];
+const aray3 = aray1 + aray2;
+console.log(typeof aray3);
+console.log(aray3);
+*/
