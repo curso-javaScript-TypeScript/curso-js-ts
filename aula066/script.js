26ad9ead82d9341d85b2d409b49b4fe5f0b82c25
Magno V Gomes <magnovgomes74@gmail.com>
2026-03-11 16:24:45 -0300

    Finalizando aula 66 - Compreendendo o método map() e suas funcionalidades

--- aula066/script.js
+++ aula066/script.js
@@ -1,13 +1,17 @@
-// Dobre os números
-//               0  1   2   3  4 .....
+/* 
+********** Filter, Map e Reduce ***************
+
+Map -> percorre o array original e retorna um novo array de mesmo 
+tamanho com os dados transformados, sem alterar a fonte original.
+
+Dobre os números
+indíces          0  1   2   3  4 .....
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const numerosEmDobro = numeros.map(valor => valor * 2);
-// console.log(numerosEmDobro);
+console.log(numerosEmDobro);
+*/
 
 // Para cada elemento:
-// Retorne apenas uma string com o nome da pessoa
-// Remova apenas a chave "nome" do objeto
-// Adicione uma chave id em cada objeto (id)
 const pessoas = [
   { nome: 'Luiz', idade: 62 },
   { nome: 'Maria', idade: 23 },
@@ -16,14 +20,21 @@ const pessoas = [
   { nome: 'Rosana', idade: 32 },
   { nome: 'Wallace', idade: 47 },
 ];
+// Retorne apenas uma string com o nome da pessoa
 const nomes = pessoas.map(obj => obj.nome);
-const idades = pessoas.map(obj => ({ idade: obj.idade }));
+console.log(nomes);
 
-const comIds = pessoas.map(function(obj, indice) {
+// Remova apenas a chave "nome" do objeto
+const idades = pessoas.map(obj => ({ idade: obj.idade }));
+console.log(idades);
+
+// Adicione uma chave id em cada objeto (id)
+
+const chaveIds = pessoas.map(function(obj, indice) {
   const newObj = { ...obj };
   newObj.id = indice;
   return newObj;
 });
 
-// console.log(pessoas);
-console.log(comIds);
+console.log(pessoas);
+console.log(chaveIds);
