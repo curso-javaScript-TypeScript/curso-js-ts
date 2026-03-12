eae81a621fd9e8235f7b233b90246a7582d389de
Magno V Gomes <magnovgomes74@gmail.com>
2026-03-12 16:28:55 -0300

    Encerrando seção 5 e aula 69 - iteração e soma com forEach

--- aula069/script.js
+++ aula069/script.js
@@ -1,8 +1,19 @@
-const a1 = [10, 20, 30];
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
 let total = 0;
 
 a1.forEach(valor => {
   total += valor;
 });
-
 console.log(total);
+
+/* 
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+a1.forEach(valor => console.log(valor));
+
+*************************************************
+
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for(let valor of a1) {
+    console.log(valor);
+  }
+*/
