601cb0f9212c0a51899a638bc9d2d57e898f0284
Magno V Gomes <magnovgomes74@gmail.com>
2026-03-13 19:14:37 -0300

    Finalizando aula 72 - domínio de Getters/Setters e variáveis privadas em Constructor Functions"

--- aula072/script.js
+++ aula072/script.js
@@ -1,15 +1,74 @@
-// defineProperty -> Getter e Setter
+/* defineProperty -> Getter e Setter
+********** GETTER/SETTER NA FACTORY FUNCTION **********
+*/
 function Produto(nome, preco, estoque) {
   this.nome = nome;
   this.preco = preco;
 
+  let estoquePrivado = estoque;
   Object.defineProperty(this, 'estoque', {
     enumerable: true, // mostra a chave
-    value: estoque, // mostra valor
-    writable: false, // pode alterar ou não
-    configurable: true // configurável mas não recomendável
+    configurable: true, // configurável mas não recomendável
+    get: function() {
+      return estoquePrivado;
+    },
+    set: function(valor) {
+      if (typeof valor !== 'number') {
+        console.log('Valor inexistente!');
+        return;
+      }
+
+      estoquePrivado = valor;
+    }
+  });
+}
+
+function criaProduto(nome) {
+  return {
+    get nome() {
+      return nome;
+    },
+    set nome(valor) {
+      nome = valor;
+    }
+  };
+}
+
+// const p2 = criaProduto('Camisa');
+// console.log(p2);
+
+const p2 = criaProduto('Camisa');
+p2.nome = 'Muda para short';
+console.log(p2.nome);
+
+
+/*  
+********** GETTER/SETTER NA CONSTRUCTOR FUNCTION **********
+
+function Produto(nome, preco, estoque) {
+  this.nome = nome;
+  this.preco = preco;
+
+  let estoquePrivado = estoque;
+  Object.defineProperty(this, 'estoque', {
+    enumerable: true, // mostra a chave
+    configurable: true, // configurável mas não recomendável
+    get: function() {
+      return estoquePrivado;
+    },
+    set: function(valor) {
+      if (typeof valor !== 'number') {
+        console.log('Valor inexistente!');
+        return;
+      }
+
+      estoquePrivado = valor;
+    }
   });
 }
 
 const p1 = new Produto('Camiseta', 20, 3);
-console.log(p1);
+// console.log(p1);
+p1.estoque = 'Valor será decidido aqui';
+console.log(p1.estoque);
+*/
