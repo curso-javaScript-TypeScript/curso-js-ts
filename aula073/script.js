b8b81d14818fa073bc55ea5ea1e474ed3073ab4d
Magno V Gomes <magnovgomes74@gmail.com>
2026-03-14 14:21:08 -0300

    Iniciando aula 73 - Explorando métodos úteis para objetos e técnicas de clonagem

---
+++ aula073/script.js
@@ -0,0 +1,13 @@
+/*
+Object.values
+Object.entries
+Object.getOwnPropertyDescriptor(o, 'prop')
+Object.assign(des, any)
+... (spread)
+
+// Já vimos
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperties (define várias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
