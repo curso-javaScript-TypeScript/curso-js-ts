dd5e0aec60221a57e9313bd2480282c4733da0ff
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-08 20:58:18 -0300

    Finalizando aula 090 - Substituição do modelo XHR pela Fetch API. Implementação de tratamento de respostas com response.text()

--- aula090/assets/js/main.js
+++ aula090/assets/js/main.js
@@ -1,4 +1,39 @@
-// Método moderno com "ASYNC", abaixo a história da evolução
+
+
+document.addEventListener('click', e => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+
+  if (tag === 'a') {
+    e.preventDefault();
+    carregaPagina(el);
+  }
+});
+
+async function carregaPagina(el) {
+  try {
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+  
+    if(response.status !== 200) throw new Error('ERRO Indefinido');
+  
+    const html = await response.text();
+    carregaResultado(html);
+    } catch(e) {
+    console.log(e);
+    }
+}
+
+function carregaResultado(response) {
+  const resultado = document.querySelector('.resultado');
+  resultado.innerHTML = response;
+}
+
+
+
+
+/* exemplo um (1)
+
 const request = obj => {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
@@ -45,3 +80,13 @@ function carregaResultado(response) {
   const resultado = document.querySelector('.resultado');
   resultado.innerHTML = response;
 }
+
+fetch('pagina1.html')
+//fetch('pagina4.html')
+  .then(resposta => {
+    if(resposta.status !== 200) throw new Error('ERRO 404 HEITA');
+    return resposta.text();
+  })
+  .then(html => console.log(html))
+  .catch(e => console.error(e));
+*/
