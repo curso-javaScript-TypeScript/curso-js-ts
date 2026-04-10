2914abbe2d30321d841d6fa8cfc4d824040052c7
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-10 19:55:40 -0300

    Encerando seção 8 - aula 091 - Implementação de consumo de dados estruturados em JSON

--- aula091/assets/js/main.js
+++ aula091/assets/js/main.js
@@ -1,3 +1,62 @@
+// fetch('pessoas.json')
+//   .then(resposta => resposta.json())
+//   .then(json => carregaDadosNaWeb(json));
+
+axios('pessoas.json')
+  .then(resposta => carregaDadosNaWeb(resposta.data));
+
+function carregaDadosNaWeb(json) {
+  const table = document.createElement('table');
+  for(let pessoa of json) {
+    const tr = document.createElement('tr');
+
+    let td = document.createElement('td');
+    td.innerHTML = pessoa.nome;    
+    tr.appendChild(td);
+
+    td = document.createElement('td');
+    td.innerHTML = pessoa.idade;
+    tr.appendChild(td);        
+
+    // td = document.createElement('td');
+    // td.innerHTML = pessoa.salario;    
+    // tr.appendChild(td);    
+
+    table.appendChild(tr);
+  }
+
+  const resultado = document.querySelector('.resultado');
+  resultado.appendChild(table);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 fetch('pessoas.json')
   .then(resposta => resposta.json())
