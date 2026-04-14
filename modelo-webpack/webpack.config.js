cc9a73337bce9b55d1439c315fce07aa21f78a81
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-14 17:28:12 -0300

    Finalizando Modelo-webpack: Configuração inicial para processamento de estilos via Webpack. Preparação do ambiente para utilização de style-loader e css-loader, permitindo a importação de arquivos CSS diretamente nos módulos JS

--- modelo-webpack/webpack.config.js
+++ modelo-webpack/webpack.config.js
@@ -2,14 +2,13 @@ const path = require('path'); // CommonJS
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js',
   },
   module: {
-    rules: [
-      {
+    rules: [{
         exclude: /node_modules/,
         test: /\.js$/,
         use: {
@@ -18,8 +17,10 @@ module.exports = {
             presets: ['@babel/env'],
           },
         },
-      },
-    ],
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   },
   devtool: 'source-map',
 };
