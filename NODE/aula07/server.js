25627ca6693d83b498d501b6e69cb5f24483fd83
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-22 19:09:09 -0300

    Iniciando aula 0 - Express + Nodemon. Configuração do Nodemon para reinicialização automática do servidor. Preparação para estudo de req.params, req.query e req.body.

---
+++ NODE/aula07/server.js
@@ -0,0 +1,33 @@
+/*
+        Criar   ler   atualizar apagar
+CRUD -> CREATE, READ, UPDATE,   DELETE
+        POST    GET   PUT       DELETE
+
+        http://meusite.com/ <- GET -> Entregue a página /
+ROTAS-> http://meusite.com/sobre <- GET -> Entregue a página /sobre
+        http://meusite.com/contato <- GET -> Entregue a página /contato
+*/
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+  <form action="/" method="POST">
+  Nome: <input type="text" name="nome">
+  <button>Enviar</button>
+  </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Recebemos o formulário');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('Obrigado por entrar em contato conosco.');
+});
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor executando na porta 3000');
+});
