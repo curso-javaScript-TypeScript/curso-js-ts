29ae2c8b241f4d2f5abba1793bbf461b8cb5da6d
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-24 13:55:06 -0300

    Finalizando aula 11 - Arquivos estáticos configurados. Pasta public validada com sucesso.

--- NODE/aula11/server.js
+++ NODE/aula11/server.js
@@ -5,8 +5,9 @@ const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 
-app.set('views', path.resolve(__dirname, 'src', 'views')); // caminho absoluto
-// app.set('views', './src/views'); <- caminho relativo
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views')); 
 app.set('view engine', 'ejs');
 
 app.use(routes);
