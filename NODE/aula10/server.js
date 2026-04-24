6a8cca433d5d472b0152694d52bcad8a04fe1064
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-24 13:14:53 -0300

    Finalizando aula 10 - Sistema de Views e renderização EJS finalizados.Implementação do res.render e configuração de caminhos absolutos  com o módulo path. Finalizando aula 10 - Sistema de Views e renderização EJS finalizados.Implementação do res.render e configuração de caminhos relativo e/ou absolutos  com o módulo path.

--- NODE/aula10/server.js
+++ NODE/aula10/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.resolve(__dirname, 'src', 'views')); // caminho absoluto
+// app.set('views', './src/views'); <- caminho relativo
+app.set('view engine', 'ejs');
+
 app.use(routes);
 
 app.listen(3000, () => {
