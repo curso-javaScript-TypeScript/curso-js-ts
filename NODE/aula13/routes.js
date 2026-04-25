67f4906ffe19929b385d3ff4fd2ae279b913dc6d
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-25 18:07:19 -0300

    Finalizando aula 13 finalizada: Implementação de Middlewares globais e de rotas com tratamento preventivo de requisições.

--- NODE/aula13/routes.js
+++ NODE/aula13/routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
+
 // Rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.rotaPost);
@@ -10,5 +11,30 @@ route.post('/', homeController.rotaPost);
 // Rotas de contato
 route.get('/contato', contatoController.contatoInicial);
 
-
 module.exports = route;
+
+
+/* ########### exemplos e testes da aula ##########
+
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
+
+function meuMiddleware(req, res, next) {
+  req.session = { nome: 'Magno', sobrenome: 'V Gomes'};
+  console.log();
+  console.log('Passei pelo seu middleware.');
+  console.log();
+  next();
+}
+
+// Rotas da home
+route.get('/', meuMiddleware, homeController.paginaInicial, function(req, res, next) {
+  console.log();
+  console.log('Não sai ainda, estou bem aqui!');
+  console.log(`'Último middleware.' Olha o que tem na req.session.sobrenome: ${req.session.sobrenome}`);
+});
+route.post('/', homeController.rotaPost);
+*/
+
