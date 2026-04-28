454a6ab5bdbf3c9a2b44551834edabeea69c2668
Magno V Gomes <magnovgomes74@gmail.com>
2026-04-28 20:07:16 -0300

    Finalizando aula 17 e seção 10. Estrutura completa do servidor com Express, MongoDB, Session, Helmet e CSRF

--- NODE/aula17/routes.js
+++ NODE/aula17/routes.js
@@ -3,7 +3,6 @@ const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
-
 // Rotas da home
 route.get('/', homeController.paginaInicial);
 route.post('/', homeController.rotaPost);
@@ -12,29 +11,3 @@ route.post('/', homeController.rotaPost);
 route.get('/contato', contatoController.contatoInicial);
 
 module.exports = route;
-
-
-/* ########### exemplos e testes da aula ##########
-
-const express = require('express');
-const route = express.Router();
-const homeController = require('./src/controllers/homeController');
-const contatoController = require('./src/controllers/contatoController');
-
-function meuMiddleware(req, res, next) {
-  req.session = { nome: 'Magno', sobrenome: 'V Gomes'};
-  console.log();
-  console.log('Passei pelo seu middleware.');
-  console.log();
-  next();
-}
-
-// Rotas da home
-route.get('/', meuMiddleware, homeController.paginaInicial, function(req, res, next) {
-  console.log();
-  console.log('Não sai ainda, estou bem aqui!');
-  console.log(`'Último middleware.' Olha o que tem na req.session.sobrenome: ${req.session.sobrenome}`);
-});
-route.post('/', homeController.rotaPost);
-*/
-
--- NODE/aula17/server.js
+++ NODE/aula17/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTIONSTRING)
   .then(() => {
     app.emit('pronto');
