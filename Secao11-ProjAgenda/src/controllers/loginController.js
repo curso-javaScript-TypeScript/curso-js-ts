473cde803b7854aa52a0f8a62470c50425132fe2
Magno V Gomes <magnovgomes74@gmail.com>
2026-05-04 13:23:50 -0300

    Seção 11 - Aula 04 - CRUD de Login: validações, persistência e controle de sessão.

--- Secao11-ProjAgenda/src/controllers/loginController.js
+++ Secao11-ProjAgenda/src/controllers/loginController.js
@@ -1,6 +1,7 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
+  if(req.session.user) return res.render('login-logado');
   res.render('login');
 };
 
@@ -21,9 +22,38 @@ exports.register = async (req, res) => {
     req.session.save(() => {
       return res.redirect('/login/index');
     });
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(() => {
+        return res.redirect('/login/index');
+      });
+      return;
+    }
+
+    req.flash('success', 'Você entrou em sua conta.');
+    req.session.user = login.user;
+    req.session.save(() => {
+      return res.redirect('/login/index');
+    });
 
   } catch (e) {
     console.log(e);
     return res.render('404');
   }
 };
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+}
--- Secao11-ProjAgenda/src/models/LoginModel.js
+++ Secao11-ProjAgenda/src/models/LoginModel.js
@@ -12,8 +12,25 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 class Login {
   constructor(body) {
     this.body = body;
-    this.errors =[];
+    this.errors = [];
     this.user = null;
+  }
+
+  async login() {
+    this.valida();
+    if(this.errors.length > 0) return;
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(!this.user) {
+      this.errors.push('Usuário não existe.');
+      return;
+    }
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida');
+      this.user = null;
+      return;
+    }
   }
 
   async register() {
@@ -27,29 +44,24 @@ class Login {
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try {  
-
-      this.user = await LoginModel.create(this.body); 
-    } catch(e) {
-      console.log(e);
-    }
+    this.user = await LoginModel.create(this.body);
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email });
-    if(user) this.errors.push('Já existe um usuário com este e-mail.');
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if(this.user) this.errors.push('Usuário já existe.');
   }
 
   valida() {
     this.cleanUp();
 
     // Validação
-    // O e-mail ter que ser válido
-    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido!');
+    // O e-mail precisa ser um válido
+    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
 
-    // A senha precisa seguir as regras disponíveis para validação
+    // A senha precisa passar pelos critérios de criação
     if(this.body.password.length < 3 || this.body.password.length > 20) {
-      this.errors.push('A senha precisa ter entre 3 e 20 caracteres');
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
     }
   }
 
