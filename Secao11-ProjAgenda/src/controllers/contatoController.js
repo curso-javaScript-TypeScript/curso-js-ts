7c6cd4cd3fcdf115815c14fab03855d1f5f4cc67
Magno V Gomes <magnovgomes74@gmail.com>
2026-05-05 16:45:58 -0300

    Seção 11 - Aula 07: CRUD completo, listagem dinâmica e refatoração da conexão Mongoose para drivers atuais.

--- Secao11-ProjAgenda/src/controllers/contatoController.js
+++ Secao11-ProjAgenda/src/controllers/contatoController.js
@@ -10,8 +10,7 @@ exports.register = async (req, res) => {
   try {
     const contato = new Contato(req.body);
     await contato.register();
-    
-    // CORREÇÃO: De 'erros' para 'errors'
+
     if(contato.errors.length > 0) {
       req.flash('errors', contato.errors);
       req.session.save(() => res.redirect('/contato/index'));
@@ -33,7 +32,6 @@ exports.editIndex = async function(req, res) {
 
   const contato = await Contato.buscaPorId(req.params.id);
   if(!contato) return res.render('404');
-
   res.render('contato', { contato });
 };
 
@@ -43,10 +41,8 @@ exports.edit = async function(req, res) {
     const contato = new Contato(req.body);
     await contato.edit(req.params.id);
       
-    // CORREÇÃO: De 'erros' para 'errors'
     if(contato.errors.length > 0) {
       req.flash('errors', contato.errors);
-      // CORREÇÃO: Redireciona de volta para a edição do ID atual em vez da index limpa
       req.session.save(() => res.redirect(`/contato/index/${req.params.id}`));
       return;
     }
@@ -58,4 +54,15 @@ exports.edit = async function(req, res) {
     console.log(e);
     res.render('404');
   }
+};
+
+exports.delete = async function(req, res) {
+  if(!req.params.id) return res.render('404');
+
+  const contato = await Contato.delete(req.params.id);
+  if(!contato) return res.render('404');
+
+  req.flash('success', 'Contato apagado com sucesso!');
+  req.session.save(() => res.redirect('/')); 
+  return;
 };
