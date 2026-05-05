dacf78fde78f84723ab07f4a48482976159bca9e
Magno V Gomes <magnovgomes74@gmail.com>
2026-05-05 18:13:55 -0300

    Seção 11 - Aula 08: Início da implementação de validação client-side via JavaScript para melhor UX.

--- Secao11-ProjAgenda/webpack.config.js
+++ Secao11-ProjAgenda/webpack.config.js
@@ -1,35 +1,21 @@
-console.log('WEBPACK CONFIG CARREGADO');
-
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-
   entry: './frontend/main.js',
-
   output: {
     path: path.resolve(__dirname, 'public', 'assets', 'js'),
     filename: 'bundle.js'
   },
-
   module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        type: 'javascript/auto'
-      },
-
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.m?js$/,
+      type: 'javascript/auto'
+      },]
   },
-
   resolve: {
     extensions: ['.js']
   },
-
   devtool: 'source-map'
 };
