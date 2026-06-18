06509eb95b97e72ac2f2c9392242a56f6628e29a
magno v gomes <magnovgomes74@gmail.com>
2026-06-18 11:33:07 -0300

    Implementei páginas de alunos, fotos, login e cadastro

--- Secao15-React-Redux-Saga-Hooks/consumir-api/src/components/Header/index.jsx
+++ Secao15-React-Redux-Saga-Hooks/consumir-api/src/components/Header/index.jsx
@@ -1,24 +1,20 @@
 import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 
 import { Nav } from './styled';
 
 export default function Header() {
-  const botaoClicado = useSelector((state) => state.example.botaoClicado);
-
   return (
     <Nav>
       <Link to="/">
         <FaHome size={24} />
       </Link>
-      <Link to="/login">
+      <Link to="/register">
         <FaUserAlt size={24} />
       </Link>
-      <Link to="/***">
+      <Link to="/login">
         <FaSignInAlt size={24} />
       </Link>
-      {botaoClicado ? 'Clicado' : 'Não clicado'}
     </Nav>
   );
 }
