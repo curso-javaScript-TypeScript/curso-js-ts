5245736ce2bf109c8e721ee80109f827f3f58746
magno v gomes <magnovgomes74@gmail.com>
2026-07-03 19:01:55 -0300

    Implementa exibição e edição de fotos dos alunos

--- Secao15-React-Redux-Saga-Hooks/consumir-api/src/pages/Fotos/index.jsx
+++ Secao15-React-Redux-Saga-Hooks/consumir-api/src/pages/Fotos/index.jsx
@@ -1,9 +1,93 @@
-import { Container } from '../../styles/GlobalStyles';
+import React from 'react';
+import { get } from 'lodash';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
-export default function Fotos() {
+import { Container } from '../../styles/GlobalStyles';
+import Loading from '../../components/Loading';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
+
+export default function Fotos(match) {
+  const dispatch = useDispatch();
+  const id = get(match, 'match.params.id', '');
+  console.log('ID:', id);
+
+  const [isLoading, setIsloading] = React.useState(false);
+  const [foto, setFoto] = React.useState('');
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsloading(true);
+
+        const { data } = await axios.get(`/alunos/${id}`);
+        setFoto(get(data, 'Fotos[0].url', ''));
+        setIsloading(false);
+      } catch {
+        toast.error('Erro ao obter imagem');
+        setIsloading(false);
+        history.push('/');
+      }
+    };
+
+    getData();
+  }, [id]);
+
+  const handleChange = async (e) => {
+    const file = e.target.files[0];
+    const fotoURL = URL.createObjectURL(file);
+
+    setFoto(fotoURL);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('foto', file);
+
+    try {
+      setIsloading(true);
+
+      await axios.post('/fotos/', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      toast.success('Foto enviada com sucesso!');
+
+      setIsloading(false);
+    } catch (err) {
+      console.log(err.response.data);
+
+      setIsloading(false);
+
+      const { status } = get(err, 'response', '');
+      toast.error('Erro ao enviar foto.');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
-      <h1>Fotos</h1>
+      <Loading isLoading={isLoading} />
+
+      <Title>Fotos</Title>
+
+      <Form>
+        <label htmlFor="foto">
+          {foto ? <img src={foto} alt="Foto" /> : 'Selecionar'}
+          <input type="file" id="foto" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Fotos.PropTypes = {
+  match: PropTypes.shape({}).isRequired,
+  history: PropTypes.shape([]).isRequired,
+};
